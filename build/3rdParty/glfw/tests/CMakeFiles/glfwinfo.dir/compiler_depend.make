030479f2c1f75946feeae00a2e69b05f452edf3a
crystalline02 <875870639@qq.com>
2024-01-04 21:02:24 +0800

    clear all

--- build/3rdParty/glfw/tests/CMakeFiles/glfwinfo.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty compiler generated dependencies file for glfwinfo.
-# This may be replaced when dependencies are built.
--- build/CMakeFiles/renderer.dir/src/texture.cpp.obj.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/renderer.dir/src/texture.cpp.obj: \
- C:\Users\ZhenKou\OneDrive\project\opengl_learn\renderer_opengl1.8\src\texture.cpp
